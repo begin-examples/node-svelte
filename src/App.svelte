9e9d16fa0b732955f71f9e82e2a3c0274c4d39da
kj <kristoferjoseph@users.noreply.github.com>
2020-02-19 17:24:01 -0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,7 @@
 <main>
   <h1>Hello {name}!</h1>
   <h2>{message}</h2>
+  <h3>Change me!</h3>
   <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -23,7 +24,8 @@
     margin: 0 auto;
   }
 
-  h1 { color: #ff3e00;
+  h1 { 
+    color: #ff3e00;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
