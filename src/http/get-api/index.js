758da7c8d97f97e2c0865f553dc35af13e9794da
Paul Chin Jr <pchinjr@gmail.com>
2020-08-13 19:09:36 -0400

    added status code

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -5,6 +5,7 @@ exports.handler = async function http (req) {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
+    statusCode: 200,
     body: JSON.stringify({
       msg: 'Hello from Svelte + your Begin API!'
     })
